4d96f0c22a3161b3d4f58060bce3d2241b6ebcc0
Ulhas Rane <ulhasvru@gmail.com>
2026-03-16 21:57:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Animated counters
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+counter.innerText = "0";
+
+const update = () => {
+
+const target = +counter.getAttribute("data-target");
+
+const current = +counter.innerText;
+
+const increment = target / 100;
+
+if(current < target){
+
+counter.innerText = `${Math.ceil(current + increment)}`;
+
+setTimeout(update,20);
+
+}else{
+
+counter.innerText = target;
+
+}
+
+};
+
+update();
+
+});
