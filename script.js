f69b060539dafbc570a5a5e81e1b59ee4dd7d144
Ulhas Rane <ulhasvru@gmail.com>
2026-03-16 22:33:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-document.querySelectorAll('nav a').forEach(anchor => {
+const counters=document.querySelectorAll(".counter");
 
-anchor.addEventListener('click', function (e) {
+counters.forEach(counter=>{
 
-e.preventDefault();
+counter.innerText="0";
 
-document.querySelector(this.getAttribute('href'))
-.scrollIntoView({
+const update=()=>{
 
-behavior: 'smooth'
+const target=+counter.getAttribute("data-target");
 
-});
+const current=+counter.innerText;
 
-});
+const increment=target/100;
+
+if(current<target){
+
+counter.innerText=`${Math.ceil(current+increment)}`;
+
+setTimeout(update,20);
+
+}
+
+else{
+
+counter.innerText=target;
+
+}
+
+};
+
+update();
 
 });
